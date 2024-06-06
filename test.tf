59c57b455bdd22d159687d43eae5064915db947d
VENKATESH ADURTHI <SESA718796@se.com>
2024-06-06 14:33:16 +0530

    added module

--- test.tf
+++ test.tf
@@ -1,21 +1,31 @@
-terraform {  
-    required_providers {    
-        azurerm = {      
-            source  = "hashicorp/azurerm"      
-            version = "~> 3.0.2"    
-            }  
-        }  
-    required_version = ">= 1.1.0"
-}
+# terraform {  
+#     required_providers {    
+#         azurerm = {      
+#             source  = "hashicorp/azurerm"      
+#             version = "~> 3.0.2"    
+#             }  
+#         }  
+#     required_version = ">= 1.1.0"
+# }
     
 provider "azurerm" {
-
         features {
-            
         }
 }
         
 resource "azurerm_resource_group" "rg" {  
     name     = "myTFResourceGroup"  
     location = "westus2"
+    tags = {
+    team     = "dopt",
+    owner    = "dopt",
+    activity = "test-TFE",
+  }
+
+}
+
+module "storage-account" {
+  source  = "app.terraform.io/DXE-POC/storage-account/azurerm"
+  version = "0.3.0"
+  resource_group_name = "rg-tfe-poc"
 }
